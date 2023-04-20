84276480fd9ecd76e210b06958f5557ad1f9c3ca
obahia <joaovictorbahia@gmail.com>
2023-04-20 13:28:02 +0100

    projeto final tt

--- modulo 5/ft2-md005.cpp
+++ modulo 5/ft2-md005.cpp
@@ -5,6 +5,7 @@ using namespace std;
 #define MAX_AUTO 20
 #define MAX_CLIENTE 20
 int N_cliente = 8;
+int N_auto = 10;
 struct Automovel
 {
     int idAuto;
@@ -15,8 +16,9 @@ struct Automovel
     string num_chassi;
 };
 Automovel automoveis[MAX_AUTO]{
-    //IdAuto/IdCliente/Marca/Modelo/Ano/Placa
-    {1, 7, "Peugeot", 308, 2020, "28Y9D"},
+    // struct do automóvel
+    // IdAuto/IdCliente/Marca/Modelo/Ano/Placa
+    {1, 7, "Peugeot", "308", 2020, "28Y9D"},
     {2, 2, "Honda", "Jazz", 2018, "97P4T"},
     {3, 6, "Audi", "A3", 2019, "55FK8"},
     {4, 3, "Seat", "Ibiza", 2020, "H1M78"},
@@ -28,7 +30,7 @@ Automovel automoveis[MAX_AUTO]{
     {10, 5, "Renault", "Megane", 2016, "3G5R7"},
 };
 
-struct Cliente
+struct Cliente // struct do cliente
 {
     int idCliente;
     string nome;
@@ -43,10 +45,9 @@ Cliente clientes[MAX_CLIENTE]{
     {5, "Carlos Torres", "carlosmt@ctt.com", 967258456},
     {6, "Miguel Castro", "migas@sapo.pt", 968124637},
     {7, "Teresa Silva", "tsmp@gmail.com", 912785423},
-    {8, "Lucinda Sá", "lmss@hotmail.com", 934254713}
-    };
+    {8, "Lucinda Sá", "lmss@hotmail.com", 934254713}};
 
-int menu_principal()
+int menu_principal() // menu principal
 {
     int op;
     cout << "\n\n========MENU PRINCIPAL========\n\n";
@@ -61,7 +62,7 @@ int menu_principal()
     return op;
 }
 
-int novo_cliente()
+int novo_cliente() // adicionar novo cliente 1
 {
     system("cls");
     if (N_cliente < MAX_CLIENTE)
@@ -83,12 +84,12 @@ int novo_cliente()
     }
 }
 
-int menu_clientes()
+int menu_clientes() // menu dos clientes
 {
     int OpC;
     system("cls");
     cout << "\n\n========MENU CLIENTES========\n\n";
-    cout << "1 - Inserir clientes\n";
+    cout << "1 - Inserir novo(s) cliente(s)\n";
     cout << "2 - Ver todos os clientes\n";
     cout << "3 - Mostrar dados do cliente pelo código\n";
     cout << "\n0 - Sair";
@@ -98,7 +99,7 @@ int menu_clientes()
     return OpC;
 }
 
-int mostrar_cliente(int id)
+int mostrar_cliente(int id) // mostrar cliente de acordo com o ID 3
 {
 
     if (id < N_cliente + 1)
@@ -116,7 +117,7 @@ int mostrar_cliente(int id)
     }
 }
 
-void mostrar_todos_clientes()
+void mostrar_todos_clientes() // função para mostrar todos os clientes
 {
     system("cls");
     cout << "\n\n";
@@ -128,13 +129,14 @@ void mostrar_todos_clientes()
     system("pause");
 }
 
-int menu_automoveis()
+int menu_automoveis() // menu dos automóveis
 {
+    system("cls");
     int OpA;
     cout << "\n\n========MENU AUTOMÓVEIS========\n\n";
     cout << "1 - Inserir novo automóvel\n";
     cout << "2 - Ver todos os automóveis\n";
-    cout << "3 - Mostrar dados de um atomóvel\n";
+    cout << "3 - Mostrar dados de um automóvel\n";
     cout << "\n0 - Sair";
     cout << "\n\n===================================\n\n";
     cout << "Opção: ";
@@ -142,15 +144,116 @@ int menu_automoveis()
     return OpA;
 }
 
+int novo_automovel() // adicionar novo carro  1
+{
+    system("cls");
+    if (N_auto < MAX_AUTO)
+    {
+
+        cout << "========NOVO AUTOMÓVEL========\n\n";
+        cout << "Insira o novo id do cliente: ";
+        cin >> automoveis[N_auto].idCliente;
+        cin.ignore();
+        cout << "Insira a marca do automóvel: ";
+        getline(cin, automoveis[N_auto].marca);
+        cout << "Insira o modelo do automóvel: ";
+        getline(cin, automoveis[N_auto].modelo);
+        cout << "Insira o ano do modelo: ";
+        cin >> automoveis[N_auto].ano;
+        cin.ignore();
+        cout << "Insira o número do chassi: ";
+        getline(cin, automoveis[N_auto].num_chassi);
+        N_auto++;
+        return 1;
+    }
+    else
+    {
+
+        return 0;
+    }
+}
+
+void mostrar_todos_automoveis() // mostrar todos os carros 2
+{
+    system("cls");
+    for (int i = 0; i < N_auto; i++)
+    {
+        cout << "(" << i + 1 << ") " << automoveis[i].idCliente << ", " << automoveis[i].marca << ", " << automoveis[i].modelo << ", " << automoveis[i].ano << ", " << automoveis[i].num_chassi << "\n\n";
+    }
+    system("pause");
+}
+
+int mostrar_automovel(int ID) // mostrar automovel de acordo com o ID do cliente 3
+{
+    int Ncarros = 0;
+    cout << "ID do cliente: " << ID << "\n\n";
+    for (int i = 0; i < N_auto; i++)
+    {
+        if (automoveis[i].idCliente == ID)
+        {
+
+            cout << "(" << automoveis[i].idAuto << ") " << automoveis[i].marca << "," << automoveis[i].ano << "," << automoveis[i].num_chassi << "\n\n";
+            Ncarros++;
+        }
+    }
+    if (Ncarros)
+    {
+        cout << "Possuí " << Ncarros << " carro(s)\n\n";
+        return 1;
+    }
+    else
+    {
+        cout << "ID NÃO ENCOTRADO!\n\n";
+    }
+
+    return 0;
+}
+
+int automovel_telefone(int tel) // mostrar automovel de acordo com o telemovel do cliente
+{
+    int Ncarros = 0, i = 0;
+    for (i = 0; i < N_cliente; i++)
+    {
+        if (tel == clientes[i].tlm)
+        {
+            mostrar_automovel(clientes[i].idCliente);
+            system ("pause");
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int matricula_ano(int ano)
+{
+
+}
+
+int menu_pesquisar() // menu pesquisar
+{
+    system("cls");
+    int OpP;
+    cout << "\n\n========MENU PESQUISAR========\n\n";
+    cout << "1 - Mostre os automóveis do cliente com um determinado telemóvel\n";
+    cout << "2 - Mostre os automóveis com matrícula igual ou anterior a um dado ano\n";
+    cout << "3 - Mostre os automóveis com matrícula igual ou posterior a um dado ano\n";
+    cout << "4 - Mostre o automóvel com um certo número de chassi\n";
+    cout << "\n0 - Sair";
+    cout << "\n\n===================================\n\n";
+    cout << "Opção: ";
+    cin >> OpP;
+    return OpP;
+}
+
 int main()
 {
     setlocale(LC_ALL, "pt_PT.utf8");
 
-    int opcao, opcao_cliente, resul, Idcliente, opcao_automovel;
+    int opcao, opcao_cliente, opcao_pesquisar, opcao_automovel, resul, Idcliente, resulA, IDauto, tlm, ANO;
 
     do
     {
-        opcao = menu_principal();
+        opcao = menu_principal(); //menu cliente
         switch (opcao)
         {
         case 1:
@@ -177,6 +280,9 @@ int main()
 
                     mostrar_todos_clientes();
 
+                    cout << "\n";
+                    break;
+
                 case 3:
                     system("cls");
                     cout << "Insira o ID para pesquisar: ";
@@ -190,19 +296,44 @@ int main()
                 }
 
             } while (opcao_cliente != 0);
-            system("cls");
+            system("cls"); 
+
+
 
             break;
         case 2:
-            system("cls");
+            system("cls"); //menu automoveis
             do
             {
                 opcao_automovel = menu_automoveis();
                 switch (opcao_automovel)
                 {
                 case 1:
-
+                    resulA = novo_automovel();
+                    if (resulA)
+                    {
+                        cout << "\nAdicionado\n";
+                    }
+                    else
+                    {
+                        cout << "Não foi possível adicionar! Limite ultrapassado!\n\n";
+                    }
+                    system("pause");
                     break;
+
+                case 2:
+                    system("cls");
+                    mostrar_todos_automoveis();
+                    break;
+
+                case 3:
+
+                    system("cls");
+                    cout << "Insira o ID do cliente para pesquisar: ";
+                    cin >> IDauto;
+                    system("cls");
+                    mostrar_automovel(IDauto);
+                    system("pause");
 
                 default:
                     break;
@@ -210,7 +341,33 @@ int main()
             } while (opcao_automovel != 0);
             system("cls");
 
+
             break;
+        case 3: //menu pesquisar
+            do
+            {
+                opcao_pesquisar = menu_pesquisar();
+                switch (opcao_pesquisar)
+                {
+                case 1:
+                    system("cls");
+                    cout << "Insira o telemóvel: ";
+                    cin >> tlm;
+                    cout << "\n";
+                    automovel_telefone(tlm);
+                    break;
+                case 2:
+                    system("cls");
+                    cout << "Insira o ano: ";
+                    cin >> ANO;  
+                    cout << "\n";
+                    matricula_ano(ANO);
+                    break;  
+
+                default:
+                    break;
+                }
+            } while (opcao_pesquisar != 0);
 
         default:
             cout << "\n\n ERRO 404 OPÇÃO INVÁLIDA  \n\nTENTE NOVAMENTE\n\n";

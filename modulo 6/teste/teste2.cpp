83aca79d3576dcb0f08337123791be4e11bce7be
obahia <joaovictorbahia@gmail.com>
2023-05-16 16:06:57 +0100

    update

--- modulo 6/teste/teste2.cpp
+++ modulo 6/teste/teste2.cpp
@@ -15,22 +15,18 @@ int main(int argc, char const *argv[])
 
     int *vetor, elemento, tamanho;
 
-    cout << "Insira o tamanho do vetor: \n\n";
+    cout << "Insira o tamanho do vetor: ";
     cin >> tamanho;
     vetor = Alocamentos(tamanho);
 
     for (int i = 0; i < tamanho; i++)
     {
-        cout << "\nDigite o valor do"
-             << "[" << i + 1 << "]"
-             << " elemento:";
+        cout << "\nDigite o valor do " << "[" << i + 1 << "]" << " elemento:";
         cin >> vetor[i];
     }
     for (int j = 0; j < tamanho; ++j)
     {
-        cout << "\n\n"
-             << vetor[j] << " ";
-
+        cout << "\n"  << vetor[j] << "\n";
     }
     delete[] vetor;
 

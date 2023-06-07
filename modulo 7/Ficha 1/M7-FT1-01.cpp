3739a7e4784e5bb8fbbd8a50d9b0ac880d30108a
João Bahia <joaovictorbahia@gmail.com>
2023-06-07 08:51:51 +0100

    Modulo 7

--- modulo 7/Ficha 1/M7-FT1-01.cpp
+++ modulo 7/Ficha 1/M7-FT1-01.cpp
@@ -12,7 +12,7 @@ struct CONTACTO
     char aniv[12];
 };
 
-int menu_principal ()
+int menu_principal()
 {
     system("cls");
     int OpP;
@@ -32,70 +32,8 @@ int menu_principal ()
 
 void ler_ficheiro()
 {
-    arquivo.open("pessoas.txt", ios::in);
-
-    if (arquivo.is_open())
-    {
-        while (getline(arquivo, linha))
-        {
-            cout << "Nome: " << linha << endl;
-
-            getline(arquivo, linha);
-            cout << "Idade: " << linha << endl;
-
-            getline(arquivo, linha);
-            cout << "Nacionalidade: " << linha << endl << "\n";
-        }
-    }
-    else
-    {
-        cout << "NAO FOI POSSIVEL ABRIR ARQUIVO\n";
-    }
-}
-void acrescentar_uma_pessoa()
-{
-}
-void apagar_e_acrescentar_uma_pessoa()
-{
-
-}
-
-int main()
-{
-    setlocale(LC_ALL, "pt_PT.utf8");
-
-    CONTACTO inform;
-    fstream arquivo;
-    char opc = 's';
+    ifstream arquivo;
     string linha;
-
-    arquivo.open("pessoas.txt", ios::out);
-    system("cls");
-    cout << "Deseja inserir um Cliente?[s/n]: ";
-    cin >> opc;
-    system("cls");
-
-    while ((opc == 's') || (opc == 'S'))
-    {
-        cin.ignore();
-        cout << "Insira um nome: ";
-        getline(cin, inform.nome);
-        arquivo << inform.nome << endl;
-        cout << "Insira a idade: ";
-        cin >> inform.idade;
-        arquivo << inform.idade << endl;
-        cin.ignore();
-        cout << "Insira a nacionalidade: ";
-        getline(cin, inform.nacio);
-        arquivo << inform.nacio << "\n";
-
-        cout << "\n\nInserir novo cliente?[s/n]";
-        cin >> opc;
-        system("cls");
-    }
-
-    arquivo.close();
-
     arquivo.open("pessoas.txt", ios::in);
 
     if (arquivo.is_open())
@@ -116,6 +54,96 @@ int main()
     {
         cout << "NAO FOI POSSIVEL ABRIR ARQUIVO\n";
     }
-
-    return 0;
 }
+void acrescentar_uma_pessoa()
+{
+    ofstream arquivo;
+    CONTACTO inform;
+
+    cout << "\t\tACRESCENTAR UMA PESSOA\t\t\n";
+
+    arquivo.open("contactos.psi.txt", ios::app);
+
+    cout << "\n\nInsira um nome: ";
+    cin >> inform.nome;
+
+    cout << "\nInsira o telemóvel: ";
+    cin >> inform.tlm;
+
+    cout << "\nInsira o aniversário: ";
+    cin >> inform.aniv;
+    cout << "\n";
+
+    arquivo << inform.nome << " || " << inform.tlm << " || " << inform.aniv << endl;
+
+    arquivo.close();
+}
+void apagar_e_acrescentar_uma_pessoa()
+{
+    ofstream arquivo;
+    char c;
+
+    cout << "Deseja apagar todo o conteúdo do ficheiro e acrescentar uma pessoa?[s/n]: ";
+    cin >> c;
+
+    if (c == 's' || c == 'S')
+    {
+        system("cls");
+        arquivo.open("contactos.psi.txt", ios::trunc);
+        cout << "\t\tCONTEÚDO APAGADO\t\n\n";
+        system("pause");
+        system("cls");
+        acrescentar_uma_pessoa();
+    }
+    else
+    {
+        system("cls");
+        cout << "\t\tNAO FOI POSSIVEL APAGAR O CONTEÚDO DO FICHEIRO\t\n\n";
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "pt_PT.utf8");
+
+      setlocale(LC_ALL, "pt_PT.utf8");
+
+   int opcao;
+
+   do
+   {
+      opcao = menu_principal();
+
+      switch (opcao)
+      {
+      case 1:
+
+         system("cls");
+         ler_ficheiro();
+         system("pause");
+         system("cls");
+
+         break;
+      case 2:
+
+         system("cls");
+         acrescentar_uma_pessoa();
+         system("pause");
+         system("cls");
+
+         break;
+      case 3:
+
+         system("cls");
+         apagar_e_acrescentar_uma_pessoa();
+         system("pause");
+         system("cls");
+
+      default:
+
+         break;
+      }
+
+   } while (opcao != 0);
+}
+   

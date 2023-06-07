04ead10b2606a1c1fe3acdfcd568eb659c62fb22
João Bahia <joaovictorbahia@gmail.com>
2023-06-07 09:08:44 +0100

    Modulo 7

--- modulo 7/Ficha 1/M7-FT1-01.cpp
+++ modulo 7/Ficha 1/M7-FT1-01.cpp
@@ -2,111 +2,115 @@
 #include <fstream>
 #include <string>
 #include <locale>
+#include <sstream>
 
 using namespace std;
 
 struct CONTACTO
 {
-    char nome[100];
-    char tlm[12];
-    char aniv[12];
+   string nome;
+   int tlm;
+   string aniv;
 };
 
 int menu_principal()
 {
-    system("cls");
-    int OpP;
-    cout << "\n\n=========================TRATAMENTO DE FICHEIROS==========================\n\n";
-    cout << "1 - Mostrar (ler) conteúdo do ficheiro\n\n";
 
-    cout << "2 - Acrescentar uma pessoa ao ficheiro\n\n";
+   int OpP;
+   cout << "\n\n=========================TRATAMENTO DE FICHEIROS==========================\n\n";
+   cout << "1 - Mostrar (ler) conteĆŗdo do ficheiro\n\n";
 
-    cout << "3 - Limpar conteúdo do ficheiro e acrescentar uma pessoa\n\n";
+   cout << "2 - Acrescentar uma pessoa ao ficheiro\n\n";
 
-    cout << "\n0 - Sair";
-    cout << "\n\n==========================================================================\n\n";
-    cout << "Opção: ";
-    cin >> OpP;
-    return OpP;
+   cout << "3 - Limpar conteĆŗdo do ficheiro e acrescentar uma pessoa\n\n";
+
+   cout << "\n0 - Sair";
+   cout << "\n\n==========================================================================\n\n";
+   cout << "Opção: ";
+   cin >> OpP;
+   return OpP;
 }
 
 void ler_ficheiro()
 {
-    ifstream arquivo;
-    string linha;
-    arquivo.open("pessoas.txt", ios::in);
+   ifstream arquivo;
+   string linha;
+   arquivo.open("contactos.psi.txt");
 
-    if (arquivo.is_open())
-    {
-        while (getline(arquivo, linha))
-        {
-            cout << "Nome: " << linha << endl;
+   if (arquivo.is_open())
+   {
+      cout << "NOME || TELEMÓVEL || ANIVERSÁRIO\n\n";
 
-            getline(arquivo, linha);
-            cout << "Idade: " << linha << endl;
+      while (getline(arquivo, linha))
+      {
+         stringstream fluxo(linha);
+         string nome, tlm, aniv;
 
-            getline(arquivo, linha);
-            cout << "Nacionalidade: " << linha << endl
-                 << "\n";
-        }
-    }
-    else
-    {
-        cout << "NAO FOI POSSIVEL ABRIR ARQUIVO\n";
-    }
+         fluxo >> nome >> tlm >> aniv;
+
+         cout << nome << '  ' << tlm << '  ' << aniv << "\n";
+      }
+
+      arquivo.close();
+   }
+   else
+   {
+      cout << "NÃO FOI POSSOSÍEL ABRIR O ARQUIVO\n";
+   }
 }
+
 void acrescentar_uma_pessoa()
 {
-    ofstream arquivo;
-    CONTACTO inform;
+   ofstream arquivo;
+   CONTACTO inform;
 
-    cout << "\t\tACRESCENTAR UMA PESSOA\t\t\n";
+   cout << "\t\tACRESCENTAR UMA PESSOA\t\t\n";
 
-    arquivo.open("contactos.psi.txt", ios::app);
+   arquivo.open("contactos.psi.txt", ios::app);
 
-    cout << "\n\nInsira um nome: ";
-    cin >> inform.nome;
+   cout << "\n\nInsira um nome: ";
+   cin >> inform.nome;
 
-    cout << "\nInsira o telemóvel: ";
-    cin >> inform.tlm;
+   cout << "\nInsira o telemóvel: ";
+   cin >> inform.tlm;
 
-    cout << "\nInsira o aniversário: ";
-    cin >> inform.aniv;
-    cout << "\n";
+   cout << "\nInsira o aniversário: ";
+   cin >> inform.aniv;
+   cout << "\n";
 
-    arquivo << inform.nome << " || " << inform.tlm << " || " << inform.aniv << endl;
+   arquivo << inform.nome << " || " << inform.tlm << " || " << inform.aniv << endl;
 
-    arquivo.close();
+   arquivo.close();
 }
+
 void apagar_e_acrescentar_uma_pessoa()
 {
-    ofstream arquivo;
-    char c;
+   ofstream arquivo;
+   char c;
 
-    cout << "Deseja apagar todo o conteúdo do ficheiro e acrescentar uma pessoa?[s/n]: ";
-    cin >> c;
+   cout << "Deseja apagar todo o conteúdo do ficheiro e acrescentar uma pessoa?[s/n]: ";
+   cin >> c;
 
-    if (c == 's' || c == 'S')
-    {
-        system("cls");
-        arquivo.open("contactos.psi.txt", ios::trunc);
-        cout << "\t\tCONTEÚDO APAGADO\t\n\n";
-        system("pause");
-        system("cls");
-        acrescentar_uma_pessoa();
-    }
-    else
-    {
-        system("cls");
-        cout << "\t\tNAO FOI POSSIVEL APAGAR O CONTEÚDO DO FICHEIRO\t\n\n";
-    }
+   if (c == 's' || c == 'S')
+   {
+      system("cls");
+      arquivo.open("contactos.psi.txt", ios::trunc);
+      cout << "\t\tCONTEĆDO APAGADO\t\n\n";
+      system("pause");
+      system("cls");
+      acrescentar_uma_pessoa();
+   }
+   else
+   {
+      system ("cls");
+      cout << "\t\tNAO FOI POSSIVEL APAGAR O CONTEĆDO DO FICHEIRO\t\n\n";
+
+   }
 }
 
 int main()
 {
-    setlocale(LC_ALL, "pt_PT.utf8");
-
-      setlocale(LC_ALL, "pt_PT.utf8");
+   setlocale(LC_ALL, "pt_PT.utf8");
 
    int opcao;
 
@@ -145,5 +149,7 @@ int main()
       }
 
    } while (opcao != 0);
+
+
+   return 0;
 }
-   
